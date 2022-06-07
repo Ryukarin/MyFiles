8ca48186f92453648e9ed30363e0da41eccdc5ac
Ryukarin <ryuukaringo@gmail.com>
2022-06-07 13:20:14 +0800

    up

---
+++ EEmountusb/README.md
@@ -0,0 +1,15 @@
+## 介绍
+
+对于EmuELEC4.3及其以上版本，外置USB设备挂载游戏ROM不识别问题的解决办法。
+
+- `ee_mountusb.sh`
+
+## 使用
+
+首先把你的USB存储设备的标卷改名为EEROMS，然后下载此脚本执行。
+
+```shell
+wget 
+sh ee_mountusb.sh
+```
+
---
+++ EEmountusb/ee_mountusb.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+mount -o remount,rw /flash
+
+USB_TYPE=`df -T | grep EEROMS | awk '{print $2}'`
+
+case $USB_TYPE in
+    "fuseblk")
+        echo "ntfs" > /flash/ee_fstype
+    	;;
+    "ntfs"|"ext4"|"exfat")
+    	$USB_TYPE > /flash/ee_fstype
+    	;;
+    *)
+        echo "vfat" > /flash/ee_fstype
+        ;;
+esac 
+
+mount -o remount,ro /flash
+
+sh /usr/bin/mount_romfs.sh
+
+echo "Please Restart EmulationStation!"
